ac5057dc1913b84f3b6546d31ab75d690848f4dd
Jeff Curtis <jeffreyscurtis@me.com>
2019-04-02 21:15:23 -0400

    update big map

--- Visits/MapViewController.swift
+++ Visits/MapViewController.swift
@@ -7,17 +7,55 @@
 //
 
 import UIKit
-
+import MapKit
+import Contacts
 class MapViewController: UIViewController {
-
+     let application = UIApplication.shared.delegate as! AppDelegate
+    @IBOutlet weak var mapSegment: UISegmentedControl!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBAction func mapButtonPressed(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = MKMapType.standard
+            break
+        case 1:
+            mapView.mapType = MKMapType.satellite
+            break
+        case 2:
+            mapView.mapType = MKMapType.hybrid
+            break
+        case 3:
+            mapView.mapType = MKMapType.mutedStandard
+            break;
+        default:
+            mapView.mapType = MKMapType.mutedStandard
+            
+        }
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.mapType = MKMapType.standard
+        self.mapSegment.selectedSegmentIndex = 0;
+        // register for notifcations on Visit Updates
+        let nc = NotificationCenter.default
+        nc.addObserver(self, selector: #selector(UpdatePlaceMark), name: Notification.Name("VisitPlaceMark"), object: nil)
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotations(application.userPlaceMarks)
+      
+        let viewRegion = MKCoordinateRegion(center: application.userPlaceMarks[0].coordinate, latitudinalMeters: 2000, longitudinalMeters: 2000)
+        mapView.setRegion(viewRegion, animated: false)
     }
     
-
-    /*
+    
+    @objc func UpdatePlaceMark(_ notification:Notification) {
+        
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotations(application.userPlaceMarks)
+       
+        
+    }    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
